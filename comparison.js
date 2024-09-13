92d290efa0c02ce59cf4fc7f55ff774d8d0017ac
deybi444 <davelorenzana26@gmail.com>
2024-09-13 10:15:40 +0800

    Create comparison.js

---
+++ comparison.js
@@ -0,0 +1,35 @@
+let firstFavNum = 26
+let secondFavNum = 19
+
+console.log("Greater than:");
+console.log(26 > 19);
+
+console.log("Less than:");
+console.log(26 < 19);
+
+console.log("Greater than or equal to:");
+console.log(26 >= 19);
+
+console.log("Less than or equal to:");
+console.log(26 <= 19);
+
+console.log("Strict equality (===):");
+console.log(26 === 19);
+
+console.log("Loose equality (==):");
+console.log(26 == 19);
+
+console.log("Strict inequality (!==):");
+console.log(26 !== 19);
+
+console.log("Loose inequality (!=):");
+console.log(26 != 19);
+
+console.log("AND:");
+console.log((26 > 19) && (19 < 26));
+
+console.log("OR:");
+console.log((26 == 19) || (19 == 26));
+
+console.log("NOT:");
+console.log(!(26 == 19));
