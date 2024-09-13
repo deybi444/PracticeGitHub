a362ef943d77d0871607198c967b3cfa006ebe26
deybi444 <davelorenzana26@gmail.com>
2024-09-13 10:34:53 +0800

    Create string.js

---
+++ string.js
@@ -0,0 +1,8 @@
+let favActorFirstName = "Paul";
+let favActorLastName = "Walker";
+let fullName = favActorFirstName + "" + favActorLastName;
+let uppercase = fullname.toUpperCase();
+let lowercase = "Britney".toLowerCase();
+let message = "My favorite actor is " + uppercase +. His best show is Fast and Furious.";
+
+console.log(message);
