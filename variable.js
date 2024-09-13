e1ae695e62219ace8814667312824a0a7ba479e9
deybi444 <davelorenzana26@gmail.com>
2024-09-13 10:02:58 +0800

    Create variable.js

---
+++ variable.js
@@ -0,0 +1,12 @@
+let name = "Dave Matthew Lardizabal";
+
+let WhatDoYouWannaBecomeInYourLife = "programmer";
+
+let gender = "male";
+
+let twitterhandle = "deybmatti_";
+
+console.log(name);
+console.log(WhatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterhandle);
