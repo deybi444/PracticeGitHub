c670fdbebabb2e4827fefb3b4e398eb1503ff7e8
deybi444 <davelorenzana26@gmail.com>
2024-09-13 10:07:24 +0800

    Create number.js

---
+++ number.js
@@ -0,0 +1,9 @@
+let firstFavNum = 26;
+let secondFavNum = 19;
+
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNum - secondFavNum);
+console.log(firstFavNum * secondFavNum);
+console.log(firstFavNum / secondFavNum);
+console.log(firstFavNum % secondFavNum);
+console.log(firstFavNum ** secondFavNum);
